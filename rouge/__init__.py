f8ca1160e7c9046df5c9bdb7aac50750982add66
Jacob Sycoff <jsycoff@berkeley.edu>
2021-04-10 14:47:06 -0700

    Add notification when loading FinRouge

--- rouge/__init__.py
+++ rouge/__init__.py
@@ -1,12 +1,8 @@
 from __future__ import absolute_import
 from rouge.rouge import FilesRouge, Rouge
 
-###Added the lines below this
-import sys
-import FinRouge
-
-sys.modules['rouge'] = FinRouge
-###And above this
+##Line below added for FinBert implementation to inform the user that they are using FinRouge and not regular rouge
+print("You have imported FinRouge. FinRouge is a modified verison of rouge for summarization of finance related documents")
 
 __version__ = "1.0.0"
 __all__ = ["FilesRouge", "Rouge"]
